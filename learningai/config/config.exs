4abd7c499bbf92aaeac5f5310fe626862c7b3eeb
RamViswanathan <raviswan1@gmail.com>
2017-11-07 07:27:05 -0800

    modified version with course content page html modified.

--- learningai/config/config.exs
+++ learningai/config/config.exs
@@ -22,15 +22,25 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
-
 config :ueberauth, Ueberauth,
     providers: [
-        github: { Ueberauth.Strategy.Github, [] }
+        github: { Ueberauth.Strategy.Github, [] },
+        google: {Ueberauth.Strategy.Google, []},
+        facebook: { Ueberauth.Strategy.Facebook, [] }
     ]
 
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
-    client_id: "39e160e4d2c6c99ef9a6",
-    client_secret: "78d9b7adea40d7fe4df82abf6147e540eb37907f"
+    client_id: System.get_env("GITHUB_CLIENT_ID"),
+    client_secret: System.get_env("GITHUB_CLIENT_SECRET")
+
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: System.get_env("GOOGLE_CLIENT_SECRET"),
+  client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
+
+
+config :ueberauth, Ueberauth.Strategy.Facebook.OAuth,
+  client_id: System.get_env("FACEBOOK_CLIENT_ID"),
+  client_secret: System.get_env("FACEBOOK_CLIENT_SECRET")
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
